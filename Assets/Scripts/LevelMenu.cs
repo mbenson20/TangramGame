e73e1894586d64fdd97091b7750b2e5b449c53b9
mbenson20 <matthewbenson17@augustana.edu>
2020-04-05 20:57:26 -0500

    Added some assets and made the level selection screen

---
+++ Assets/Scripts/LevelMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelMenu : MonoBehaviour
+{
+    public void Back()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void PlayLevel(int levelNum)
+    {
+        SceneManager.LoadScene(levelNum + 1);
+    }
+}
